f4ba860d78b45517f680933140d271315f588ebf
Cody Kimb <codykimbell@gmail.com>
2021-05-12 14:34:34 -0500

    Update the auth middleware function to work with the GraphQL API

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,7 +2,29 @@
 const { gql } = require('apollo-server-express');
 
 // create our typeDefs
-const typeDefs = gql``;
+const typeDefs = gql`
+
+type Book {
+    _id: ID
+    authors: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+type Query {
+    books: [Book]
+  }
+
+type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
+
 
 // export the typeDefs
 module.exports = typeDefs;
